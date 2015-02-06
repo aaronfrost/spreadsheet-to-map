300b80a64b9acbf08c3fbe13423a30c3b07ae6b3
Aaron-Frost <frost@domo.com>
2015-02-05 20:25:48 -0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,69 @@
+/**
+ *
+ * This code was skeletoned in from http://www.nczonline.net/blog/2014/03/04/accessing-google-spreadsheets-from-node-js/
+ *
+ */
+
+var Spreadsheet = require('edit-google-spreadsheet');
+var _ = require('lodash');
+var fs = require('fs');
+var env = require('./env');
+
+Spreadsheet.load({
+  debug: true,
+  spreadsheetId: '1JtEboCtObZbD4df5Wdl2kDFSMhmO2kBGT51Z2JEVyS4',
+  worksheetId: 'out6gx0',
+
+  oauth : {
+    email: env.serviceAccount,
+    keyFile: './key-file.pem'
+  }
+
+}, function sheetReady(err, spreadsheet) {
+
+  if (err) {
+    throw err;
+  }
+
+  spreadsheet.receive(function(err, rows, info) {
+    if (err) {
+      throw err;
+    }
+
+    var allRows = _.chain(rows)
+      .map(function(d){
+        return d;
+      })
+      .map(function(d){
+        return new Row(d);
+      })
+      .value();
+
+    var publicEvents = _.filter(allRows, function(r){
+      return !r.private;
+    });
+    console.log('rows', allRows.length, publicEvents.length);
+    fs.writeFile('public/events.json', JSON.stringify(publicEvents), function(e, res){
+      if(e) console.error(e);
+      console.log('RES', res);
+    });
+  });
+
+});
+
+function Row(data){
+  if(this == global) return new Row(data);
+
+  this.time = data[1];
+  this.fullName = data[2];
+  this.email = data[3];
+  this.ticketLink = data[4] || "";
+  if(!data[4].match(/^http/)){
+    this.private = true;
+  }
+  this.locationName = data[5];
+  this.lat = data[6] || 0;
+  this.long = data[7] || 0;
+  this.live = data[8];
+
+}
